b229cd925411cc4533bd5f1e4177afa4aedae842
Ard Westerveld <a.j.westerveld@saxion.nl>
2024-04-22 09:53:49 +0000

    add sim option to main launch file

--- smart_diffbot_bringup/launch/main.launch.py
+++ smart_diffbot_bringup/launch/main.launch.py
@@ -1,8 +1,10 @@
 import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
 
 
 robot = 'smart_diffbot'
@@ -10,9 +12,28 @@ robot = 'smart_diffbot'
 
 def generate_launch_description():
 
+    ## arguments
+    sim = LaunchConfiguration('sim')
+
+    declared_arguments =[]
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'sim',
+            default_value='true',
+            description='Run robot in simulation (sim:=true) or use real hardware (sim:=false)'
+        )
+    )
+
     ## Launch simulation
     launch_simulation = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(robot+'_bringup'), 'launch', 'simulation.launch.py')]),
+        condition=IfCondition(sim),
+    )
+
+    ## Launch real hardware
+    launch_hardware = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(robot+'_bringup'), 'launch', 'hardware.launch.py')]),
+        condition=UnlessCondition(sim),
     )
 
     ## Launch control
@@ -33,10 +54,11 @@ def generate_launch_description():
 
 
     ## Launch description
-    return LaunchDescription([
+    return LaunchDescription(declared_arguments + [
 
         # Launch
         launch_simulation,
+        launch_hardware,
         launch_control,
         launch_localization,
         launch_navigation,
