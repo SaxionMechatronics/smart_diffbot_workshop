7649a8a24a5076add81eeecf38af08fa4ca81ea1
Ard Westerveld <a.j.westerveld@saxion.nl>
2024-09-10 16:11:20 +0200

    add sim argument for launch file and xacro

--- smart_diffbot_bringup/launch/main.launch.py
+++ smart_diffbot_bringup/launch/main.launch.py
@@ -3,23 +3,42 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
-
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
+from launch.actions import DeclareLaunchArgument
 
 robot = 'smart_diffbot'
 
 
 def generate_launch_description():
+    ## Arguments
+    sim_arg = DeclareLaunchArgument(
+            'sim',
+            default_value='true',
+            description='Run robot in simulation (sim:=true) or use real hardware (sim:=false)'
+        )
+
+    sim = LaunchConfiguration('sim')
 
     ## Launch simulation
     launch_simulation = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(robot+'_bringup'), 'launch', 'simulation.launch.py')]),
+        condition=IfCondition(sim),
+    )
+
+    ## Launch actual hardware
+    launch_hardware = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(robot+'_bringup'), 'launch', 'hardware.launch.py')]),
+        condition=UnlessCondition(sim),
     )
  
  
     ## Launch description
     return LaunchDescription([
+        sim_arg,
 
         # Launch
         launch_simulation,
+        launch_hardware
     ])
     
