1ce905d0d0e76c553cfe393c51aa75ce04ac524b
Ard Westerveld <a.j.westerveld@saxion.nl>
2025-11-28 15:17:52 +0000

    update robot dimensions to match actual hardware

--- smart_diffbot_bringup/launch/simulation.launch.py
+++ smart_diffbot_bringup/launch/simulation.launch.py
@@ -10,7 +10,7 @@ from ament_index_python.packages import get_package_share_directory
 
 robot = 'smart_diffbot'
 world = 'docking_world'
-world = 'park_world'
+# world = 'park_world'
 
 def generate_launch_description():
     
